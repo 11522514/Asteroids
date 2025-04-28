d330c03b27acdc3a1cd66dcca246da83feb2809c
11522514 <fengkeven@yahoo.com>
2025-04-27 22:26:30 -0700

    Implemented player movement, asteroids, and shooting without asteroid scoring

--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     print("Starting Asteroids!")
@@ -10,12 +15,32 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color(0,0,0,0))
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collision_detection(asteroid):
+                print("Game over!")
+                sys.exit()
+        for thing in drawable:
+            thing.draw(screen)
+        dt = game_clock.tick(60) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
